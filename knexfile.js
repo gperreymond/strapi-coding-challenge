7d50edae00fb7603fcb5732af3e52f37507530ff
Gilles Perreymond <gperreymond@gmail.com>
2019-11-24 17:22:41 +0100

    feat(): add db config for demo and production

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,29 @@
 const { postgres } = require('./config')
 
+const conn = {
+  client: 'pg',
+  version: '9.6',
+  connection: {
+    host: postgres.hostname,
+    port: postgres.port,
+    user: postgres.username,
+    password: postgres.password,
+    database: 'infra'
+  }
+}
+
 module.exports = {
   development: {
-    client: 'pg',
-    version: '9.6',
-    connection: {
-      host: postgres.hostname,
-      port: postgres.port,
-      user: postgres.username,
-      password: postgres.password,
-      database: 'infra'
-    },
+    ...conn,
     migrations: {
       directory: './data/migrations'
     },
     seeds: { directory: './data/seeds' }
+  },
+  demo: {
+    ...conn
+  },
+  production: {
+    ...conn
   }
 }
